d434ceb94879a5e47686970060dc1defb42c63c1
Georgina <faithwangare750@gmail.com>
2023-03-18 13:26:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Write your solution here!
 let cats =["Milo", "Otis", "Garfield" ];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  
 function destructivelyAppendCat(name) {
     cats.push("Ralph");
 
